8154a7d896903e16e3f81a5454a4d8bd130203d0
Tommy Lynge Jørgensen <tlyngej@gmail.com>
2018-03-18 17:41:18 +0100

    Add some basic methods to DockerComposeConnection class

--- src/DockerComposeConnection.php
+++ src/DockerComposeConnection.php
@@ -10,4 +10,32 @@ namespace App;
 class DockerComposeConnection extends SSHConnection
 {
 
+    protected $project;
+
+    /**
+     * @return mixed
+     */
+    public function getProject()
+    {
+        return $this->project;
+    }
+
+    /**
+     * @param mixed $project
+     */
+    public function setProject($project): void
+    {
+        $this->project = $project;
+    }
+
+
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+
+    }
 }
